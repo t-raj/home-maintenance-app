b37aa035adf4c0117a4b4fbab79a1cff336f9205
Bethany Childs <BethanyChilds@gmail.com>
2017-10-16 10:41:25 -0500

    fixed data validation in Heater

--- Heater.java
+++ Heater.java
@@ -15,14 +15,17 @@ public class Heater extends AddComponents {
         System.out.println("Is your heating system radiators or a furnace?");
         String heat = user.nextLine();
         heat = heat.toLowerCase();
+        while (!heat.equals("radiators") && !heat.equals("radiator") && !heat.equals("furnace")) {
+            System.out.println("Please input 'radiator' or 'heater' after the prompt");
+            System.out.println("Is your heating system radiators or a furnace?");
+            heat = user.nextLine();
+            heat = heat.toLowerCase();
+        }
         user.close();
         if (heat.equals("radiators") || heat.equals("radiator")) {
             instructions = "When your radiators start in the Fall, check all around for leaks. If you notice any let your landlord know ASAP!";
-        } else if (heat.equals("furnace")) {
-            instructions = "Filters have to be changed every 3 months. The easiest way to tell what type of filter you have is to look for a part number or size displayed somewhere on the filter. Most manufacturers will put their part number or nominal size on the outside of the filter for easy identification.";
         } else {
-            System.out.println("Please input 'radiator' or 'heater' after the prompt");
-            this.setNotes();
+            instructions = "Filters have to be changed every 3 months. The easiest way to tell what type of filter you have is to look for a part number or size displayed somewhere on the filter. Most manufacturers will put their part number or nominal size on the outside of the filter for easy identification.";
         }
         this.notes = instructions;
     }

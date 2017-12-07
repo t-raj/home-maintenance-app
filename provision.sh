bab79e3a75ccfc0c63b0f0c2af98052e38cf0b19
BethanyChilds <bchilds@luc.edu>
2017-12-07 17:56:15 +0000

    provision.sh edited online with Bitbucket

--- provision.sh
+++ provision.sh
@@ -21,36 +21,33 @@ function setup_daemon {
    cd systemd
    mkdir system
    cd /
-   mv /tmp/app/$service_file /etc/systemd/system/$service_file #move our app to systemD TODO move all classes
+   mv /tmp/$service_file /etc/systemd/system/$service_file
    systemctl enable $service_file
    systemctl start $service_file
 }
 
 function change_ownership {
   user=$1
-  foldername=$2
   #change the ownership of the folder and files in the folder
   #-R makes it recursive
-  echo "user: " $user "will now have ownership of: " $foldername
-  chown -R $user /opt/$foldername
+  echo "user: " $user "will now have ownership of opt/app"
+  chown -R $user /opt/app
 }
 
 function untar_application {
-  tarfile=$1
   #untar from /tmp
   cd /tmp
   tar -zxvf app.tar.gz
   cd /
-  #mv app/* to /opt/<appname>
-  mv /tmp/app/$tarfile/* /opt/$tarfile
+  #mv app/* to /opt/
+  mv /tmp/app/* /opt/app/*
 }
 
 function main_daemon {
-  app_name="menu.java"
   user="app.svc"
   create_service_user $user
-  untar_application $app_name
-  change_ownership $user $app_name
+  untar_application
+  change_ownership $user
   # move daemon config flask_app.service to correct spot in function below
   setup_daemon "app.service"
 }

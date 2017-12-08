816071594757dae716b9e16b4e767815dfcbf9a2
BethanyChilds <bchilds@luc.edu>
2017-12-08 00:48:29 +0000

    non_generated_destroy_infra.sh edited online with Bitbucket

--- project_example/non_generated_destroy_infra.sh
+++ project_example/non_generated_destroy_infra.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#This is like the cr4pi3st way to write this, but it works for the project
-#Please make this nicer for your projects by at least writing some functions
-
+#reading data from output of create_infra.sh
 ec2instance_id=""
 security_group_id=""
 privsub_id=""
 pubsub_id=""
-pubsub_id=""
+pubsub_id="" #don't think we need this one without an RDS
 priv_route_id=""
 igw_id=""
 vpc=""
 natgw_id=""
 
+#destroying infrastructure
 aws ec2 terminate-instances --instance-ids ${ec2instance_id}
 aws ec2 delete-security-group --group-id ${security_group_id}
 aws ec2 delete-subnet --subnet-id ${privsub_id}
 aws ec2 delete-subnet --subnet-id ${pubsub_id}
-#NOTE: If you create a public subnet and route for RDS please add it in here
 aws ec2 delete-route-table --route-table-id ${pub_route_id}
 aws ec2 delete-route-table --route-table-id ${priv_route_id}
 aws ec2 detach-internet-gateway --internet-gateway-id ${igw_id} --vpc-id ${vpc}

9c4fbd814dbcc442e09f1d1d203e4df51b25de11
Bethany Childs <BethanyChilds@gmail.com>
2017-12-08 18:21:31 -0600

    moved jar file

---
+++ project_example/awesome_app/test/LightBulbsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LightBulbsTest {
+
+    @Test
+    public void getNotes() throws Exception {
+    }
+
+    @Test
+    public void setNotes() throws Exception {
+    }
+
+}

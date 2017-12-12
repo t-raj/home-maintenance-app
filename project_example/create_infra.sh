f690ced9d3d0d2598ee5bcab87728e091b1a6ca0
Childs <bchilds@lmb-16017.local>
2017-12-12 15:55:31 -0600

    edited down setup AWS file

--- project_example/create_infra.sh
+++ project_example/create_infra.sh
@@ -2,10 +2,9 @@
 
 region="us-west-2"
 vpc_block="10.0.0.0/16"
-profile="lanerda"
-public_subnet="10.0.1.0/24"
+profile="home"
 private_subnet="10.0.0.0/24"
-main_tag="Intro_Home_App"
+main_tag="Home_App"
 endfile="all_instances.txt"
 destroy_file="generated_destroy_infra.sh"
 
@@ -23,37 +22,12 @@ read $vpc
 echo -n "VPC: $vpc\n" >> $endfile
 aws ec2 create-tags --resources $vpc --tags Key=Name, Value=${main_tag} --profile ${profile}
 
-# Create All Subnets
-aws ec2 create-subnet --vpc-id ${vpc} --cidr-block ${public_subnet}
+# Create Subnet
 aws ec2 create-subnet --vpc-id ${vpc} --cidr-block ${private_subnet}
-echo -n "Please enter the public_subnet id"
-read $pubsub_id
-echo -n "PUBSUBSID: $pubsub_id\n" >> $endfile
-aws ec2 create-tags --resources $pubsub_id --tags Key=Name, Value=${main_tag} --profile ${profile}
 echo -n "Please enter the private_subnet id"
 read $privsub_id
 echo -n "PRIVSUBID: $privsub_id\n" >> $endfile
 aws ec2 create-tags --resources $privsub_id --tags Key=Name, Value=${main_tag} --profile ${profile}
-
-
-# Create IGW
-aws ec2 create-internet-gateway --region ${region} --profile ${profile}
-echo -n "Please enter the igw id"
-read $igw_id
-echo -n "IGWID: $igw_id\n" >> $endfile
-aws ec2 attach-internet-gateway --vpc-id $vpc --internet-gateway-id $igw_id --profile ${profile} --region ${region}
-aws ec2 create-tags --resources $igw_id --tags Key=Name, Value=${main_tag} --profile ${profile}
-
-#Create Custom Route Table for VPC and Assign to IGW
-aws ec2 create-route-table --vpc-id $vpc --region ${region} --profile ${profile}
-echo -n "Please enter the route table id"
-read $pub_route_id
-echo -n "PUBROUTEID: $pub_route_id\n" >> $endfile
-aws ec2 create-route --route-table-id $pub_route_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id --profile ${profile} --region ${region}
-aws ec2 create-tags --resources $pub_route_id --tags Key=Name, Value=public_${main_tag} --profile ${profile}
-aws ec2 associate-route-table  --subnet-id $pubsub_id --route-table-id $pub_route_id --profile ${profile} --region ${region}
-aws ec2 describe-route-tables --route-table-id $pub_route_id  --profile ${profile} --region ${region}
-
 
 # Create Private RouteTable and Associate it to private subnet
 aws ec2 create-route-table --vpc-id $vpc --region ${region} --profile ${profile}
@@ -63,21 +37,6 @@ echo -n "PRIVROUTEID: $priv_route_id\n" >> $endfile
 aws ec2 create-tags --resources $priv_route_id --tags Key=Name, Value=private_${main_tag} --profile ${profile} --region ${region}
 aws ec2 associate-route-table  --subnet-id $privsub_id --route-table-id $priv_route_id --profile ${profile} --region ${region}
 aws ec2 describe-route-tables --route-table-id $priv_route_id  --profile ${profile} --region ${region}
-
-# Create NATGateway and ElasticIP as well as Associate to Private Route Table
-aws ec2 allocate-address  --profile ${profile} --region ${region}
-echo -n "Please enter the elasticip id"
-read $elastic_id
-echo -n "ELASTICID: $elastic_id\n" >> $endfile
-aws ec2 create-nat-gateway --profile ${profile} --region ${region}
-echo -n "Please enter the nat gateway id"
-read $natgw_id
-echo -n "NATGWID: $natgw_id\n" >> $endfile
-aws ec2 create-tags --resources $natgw_id --profile ${profile} --region ${region}
-aws ec2 create-nat-gateway --subnet-id $pubsub_id --allocation-id $elastic_id
-aws ec2 create-route --route-table-id $priv_route_id --destination-cidr-block 0.0.0.0/0 --gateway-id $natgw_id --profile ${profile} --region ${region}
-
-
 
 # Create 1 EC2 Instance
 aws ec2 create-key-pair --key-name DemoKeyPair --query 'KeyMaterial' --output text > DemoKeyPair.pem
@@ -101,10 +60,5 @@ aws ec2 create-tags --resources $ec2instance_id --profile ${profile} --region ${
 echo -n "aws ec2 terminate-instances --instance-ids ${ec2instance_id}\n" >> $destroy_file
 echo -n "aws ec2 delete-security-group --group-id $security_group_id\n" >> $destroy_file
 echo -n "aws ec2 delete-subnet --subnet-id $privsub_id\n" >> $destroy_file
-echo -n "aws ec2 delete-subnet --subnet-id $pubsub_id\n" >> $destroy_file
-echo -n "aws ec2 delete-route-table --route-table-id $pub_route_id\n" >> $destroy_file
 echo -n "aws ec2 delete-route-table --route-table-id $priv_route_id\n" >> $destroy_file
-echo -n "aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc\n" >> $destroy_file
-echo -n "aws ec2 delete-internet-gateway --internet-gateway-id $igw_id\n" >> $destroy_file
-echo -n "aws ec2 delete-nat-gateway --nat-gateway-id $natgw_idn" >> $destroy_file
 echo -n "aws ec2 delete-vpc --vpc-id $vpc\n" >> $destroy_file
